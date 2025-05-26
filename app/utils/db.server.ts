561b040d00cca813678d4e9b823b6a2e7c64f360
MarziaYarzada <marzaiyarzada2@gmail.com>
2025-05-26 18:27:19 +0430

    - added Prisma client setup with proper singleton pattern for development

---
+++ app/utils/db.server.ts
@@ -0,0 +1,16 @@
+//app/db.server.ts
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prismaGlobal: PrismaClient;
+}
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.prismaGlobal) {
+    global.prismaGlobal = new PrismaClient();
+  }
+}
+
+const prisma = global.prismaGlobal ?? new PrismaClient();
+
+export default prisma;
